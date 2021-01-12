3dd00e595ed9f4bab0f6aec3b8e13339e04f3a8b
Nick Amin <amin.nj@gmail.com>
2021-01-12 01:04:23 -0800

    fix issue 1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = "1.6.3"
+__version__ = "1.6.4"
 
 here = path.abspath(path.dirname(__file__))
 
