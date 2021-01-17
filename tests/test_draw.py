0a8c98d449bfcba35f9206634613185d3cbf40d6
Nick Amin <amin.nj@gmail.com>
2021-01-16 22:20:50 -0800

    consolidate testing

--- tests/test_draw.py
+++ tests/test_draw.py
@@ -40,16 +40,6 @@ def df_jagged():
     return awkward1_arrays_to_dataframe(a)
 
 
-def drawclose(df, varexp, sel, y):
-    if df is None:
-        df = df_jagged()
-    x = tree_draw_to_array(df, varexp, sel)
-    x = np.array(x)
-    y = np.array(y)
-    assert x.shape == y.shape
-    np.testing.assert_allclose(x, y)
-
-
 def test_draw_1d(df_flat):
     df = df_flat
     c1 = tree_draw(df, "a+b", "(a<b<c) and (a<0.5)", bins="10,0,2").counts
@@ -97,11 +87,6 @@ def test_jitdraw_1d(df_flat):
     np.testing.assert_allclose(c1, c2)
 
 
-def test_broadcasting(df_jagged):
-    df = df_jagged
-    drawclose(df, "Jet_pt", "", [42.0, 15.0, 10.5, 11.5, 50.0, 5.0])
-
-
 def test_draw_to_hist1d(df_jagged):
     df = df_jagged
     assert df.draw("Jet_pt").integral == 6
@@ -115,111 +100,64 @@ def test_draw_to_hist2d(df_jagged):
     assert h.integral == 4
 
 
-def test_broadcasting(df_jagged):
-    df = df_jagged
-    drawclose(df, "Jet_pt", "", [42.0, 15.0, 10.5, 11.5, 50.0, 5.0])
-    drawclose(df, "MET_pt", "", [46.5, 30.0, 82.0, 8.9])
-    drawclose(df, "Jet_pt", "Jet_pt > 40", [42.0, 50.0])
-    drawclose(df, "MET_pt", "MET_pt > 40", [46.5, 82.0])
-    drawclose(df, "Jet_pt", "MET_pt > 40", [42, 15, 10.5, 11.5])
-    drawclose(df, "Jet_pt", "abs(Jet_eta) > 1 and MET_pt > 10", [42.0, 11.5])
-
-
-def test_2d(df_jagged):
-    df = df_jagged
-    drawclose(
-        df,
-        "Jet_pt:Jet_eta",
-        "MET_pt > 40.",
-        [[42, -2.2], [15, 0.4], [10.5, 0.5], [11.5, 1.5]],
-    )
-
-
-def test_correct_value_picked_out(df_jagged):
-    df = df_jagged
-    drawclose(df, "Jet_eta", "Jet_pt > 40 and MET_pt > 40", [-2.2])
-    drawclose(df, "Jet_eta + 1", "Jet_pt > 40 and MET_pt > 40", [-1.2])
-
-
-def test_reductions(df_jagged):
-    df = df_jagged
-    drawclose(df, "max(abs(Jet_eta))", "", [2.2, 1.5, 3.0])
-    drawclose(df, "max(abs(Jet_eta))", "MET_pt > 80", [1.5])
-    drawclose(df, "min(Jet_pt)", "", [10.5, 11.5, 5.0])
-    drawclose(
-        df, "mean(Jet_pt)", "", [1.0 / 3 * (42 + 15 + 10.5), 11.5, 0.5 * (50 + 5)]
-    )
-    drawclose(df, "length(Jet_pt)", "", [3, 0, 1, 2])
-    drawclose(df, "length(Jet_pt)", "MET_pt < 10", [2])
-    drawclose(df, "sum(Jet_pt)", "MET_pt < 10", [50 + 5])
-
-
-def test_indexing(df_jagged):
-    df = df_jagged
-    drawclose(df, "Jet_pt[Jet_pt>25]", "", [42, 50])
-    drawclose(df, "Jet_pt[2]", "", [10.5])
-    drawclose(df, "Jet_pt[0]:Jet_pt[1]", "", [[42, 15], [50, 5]])
-    drawclose(df, "Jet_pt[0]:Jet_pt[1]", "MET_pt > 40", [[42, 15]])
-
-
-def test_indexing_reduction(df_jagged):
-    df = df_jagged
-    drawclose(df, "sum(Jet_pt[abs(Jet_eta)<2.0])", "", [15 + 10.5, 0.0, 11.5, 50.0])
-
-
-def test_counting(df_jagged):
-    df = df_jagged
-    drawclose(df, "sum(Jet_pt>10)", "MET_pt>40", [3, 1])
-
-
-def test_mathematical_operations(df_jagged):
-    df = df_jagged
-    drawclose(df, "np.exp(sum(Jet_pt>10))", "MET_pt>40", [np.exp(3), np.exp(1)])
-
-
-def test_comparisons(df_jagged):
-    df = df_jagged
-    drawclose(df, "Jet_pt", "(14. < Jet_pt < 16.)", [15])
-    drawclose(df, "Jet_pt", "(14. < Jet_pt) and (Jet_pt < 16.)", [15])
-    drawclose(df, "Jet_pt", "(14. < Jet_pt) & (Jet_pt < 16.)", [15])
-
-
-def test_negation(df_jagged):
-    df = df_jagged
-    drawclose(df, "Jet_pt", "not(14. < Jet_pt < 16.)", [42, 10.5, 11.5, 50, 5])
-    drawclose(df, "Jet_pt", "~(14. < Jet_pt < 16.)", [42, 10.5, 11.5, 50, 5])
-    drawclose(df, "not MET_pt>40", "", [False, True, False, True])
-
-
-def test_slicing(df_jagged):
-    df = df_jagged
-    drawclose(df, "sum(Jet_pt[:2])", "", [42 + 15, 0, 11.5, 50 + 5])
-    drawclose(df, "sum(Jet_pt[2:3])", "MET_pt > 40", [10.5, 0.0])
-    drawclose(df, "sum(Jet_pt[(Jet_pt>40) and abs(Jet_eta)<2.4])", "", [42, 0, 0, 50])
-    drawclose(
-        df, "sum(Jet_pt[(Jet_pt>40) and abs(Jet_eta)<2.4])", "MET_pt > 40", [42, 0]
-    )
-
-
-def test_nested(df_jagged):
-    df = df_jagged
-    drawclose(
-        df, "length(Jet_pt) == 2 and sum(Jet_pt) > 40", "", [False, False, False, True]
-    )
-    drawclose(
-        df,
-        "(MET_pt>40) and sum((Jet_pt>40) and (abs(Jet_eta)<2.4)) >= 1",
-        "",
-        [True, False, False, False],
-    )
-
-
-def test_booleans(df_jagged):
-    df = df_jagged
-    drawclose(df, "(MET_pt>30) or False", "", [True, False, True, False])
-    drawclose(df, "(MET_pt>30) and False", "", [False, False, False, False])
-    drawclose(df, "(MET_pt>30) or True", "", [True, True, True, True])
-    drawclose(df, "(MET_pt>30) and True", "", [True, False, True, False])
+@pytest.mark.parametrize(
+    "varexp,sel,expected",
+    [
+        ("Jet_pt", "", [42.0, 15.0, 10.5, 11.5, 50.0, 5.0]),
+        ("Jet_pt", "abs(Jet_eta) > 1 and MET_pt > 10", [42.0, 11.5]),
+        ("Jet_pt", "MET_pt > 40", [42, 15, 10.5, 11.5]),
+        ("MET_pt", "MET_pt > 40", [46.5, 82.0]),
+        ("Jet_pt", "Jet_pt > 40", [42.0, 50.0]),
+        ("MET_pt", "", [46.5, 30.0, 82.0, 8.9]),
+        ("Jet_pt", "", [42.0, 15.0, 10.5, 11.5, 50.0, 5.0]),
+        ("Jet_eta + 1", "Jet_pt > 40 and MET_pt > 40", [-1.2]),
+        ("Jet_eta", "Jet_pt > 40 and MET_pt > 40", [-2.2]),
+        ("sum(Jet_pt)", "MET_pt < 10", [50 + 5]),
+        ("length(Jet_pt)", "MET_pt < 10", [2]),
+        ("length(Jet_pt)", "", [3, 0, 1, 2]),
+        ("mean(Jet_pt)", "", [1.0 / 3 * (42 + 15 + 10.5), 11.5, 0.5 * (50 + 5)]),
+        ("min(Jet_pt)", "", [10.5, 11.5, 5.0]),
+        ("max(abs(Jet_eta))", "MET_pt > 80", [1.5]),
+        ("max(abs(Jet_eta))", "", [2.2, 1.5, 3.0]),
+        ("Jet_pt[0]:Jet_pt[1]", "MET_pt > 40", [[42, 15]]),
+        ("Jet_pt[0]:Jet_pt[1]", "", [[42, 15], [50, 5]]),
+        ("Jet_pt[2]", "", [10.5]),
+        ("Jet_pt[Jet_pt>25]", "", [42, 50]),
+        ("sum(Jet_pt[abs(Jet_eta)<2.0])", "", [15 + 10.5, 0.0, 11.5, 50.0]),
+        ("sum(Jet_pt>10)", "MET_pt>40", [3, 1]),
+        ("np.exp(sum(Jet_pt>10))", "MET_pt>40", [np.exp(3), np.exp(1)]),
+        ("Jet_pt", "(14. < Jet_pt) & (Jet_pt < 16.)", [15]),
+        ("Jet_pt", "(14. < Jet_pt) and (Jet_pt < 16.)", [15]),
+        ("Jet_pt", "(14. < Jet_pt < 16.)", [15]),
+        ("not MET_pt>40", "", [False, True, False, True]),
+        ("Jet_pt", "~(14. < Jet_pt < 16.)", [42, 10.5, 11.5, 50, 5]),
+        ("Jet_pt", "not(14. < Jet_pt < 16.)", [42, 10.5, 11.5, 50, 5]),
+        ("(MET_pt>30) and True", "", [True, False, True, False]),
+        ("(MET_pt>30) or True", "", [True, True, True, True]),
+        ("(MET_pt>30) and False", "", [False, False, False, False]),
+        ("(MET_pt>30) or False", "", [True, False, True, False]),
+        ("length(Jet_pt) == 2 and sum(Jet_pt) > 40", "", [False, False, False, True]),
+        ("sum(Jet_pt[(Jet_pt>40) and abs(Jet_eta)<2.4])", "MET_pt > 40", [42, 0]),
+        ("sum(Jet_pt[(Jet_pt>40) and abs(Jet_eta)<2.4])", "", [42, 0, 0, 50]),
+        ("sum(Jet_pt[2:3])", "MET_pt > 40", [10.5, 0.0]),
+        ("sum(Jet_pt[:2])", "", [42 + 15, 0, 11.5, 50 + 5]),
+        (
+            "Jet_pt:Jet_eta",
+            "MET_pt > 40.",
+            [[42, -2.2], [15, 0.4], [10.5, 0.5], [11.5, 1.5]],
+        ),
+        (
+            "(MET_pt>40) and sum((Jet_pt>40) and (abs(Jet_eta)<2.4)) >= 1",
+            "",
+            [True, False, False, False],
+        ),
+    ],
+)
+def test_draw(df_jagged, varexp, sel, expected):
+    x = tree_draw_to_array(df_jagged, varexp, sel)
+    x = np.array(x)
+    y = np.array(expected)
+    np.testing.assert_allclose(x, y)
 
 
 if __name__ == "__main__":
